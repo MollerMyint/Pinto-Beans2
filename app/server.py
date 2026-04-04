da8b225d219a90af2d1431ead1326d6882f8d2fb
jessp60 <jmpinto@cpp.edu>
2026-04-03 23:08:29 -0700

    implemented signup and log in pages

--- app/server.py
+++ app/server.py
@@ -11,40 +11,34 @@ app = Flask(__name__)
 # Load the dotenv file 
 load_dotenv()
 
-mycursor = None 
-pwd = None
-s = None
+# Set up the database connection 
+try:
+    
+    # Grab environment variables from the dotenv file
+    mydb = mysql.connector.connect(
+        host=os.getenv("DB_HOSTNAME"),
+        user=os.getenv("DB_USERNAME"),
+        password=os.getenv("DB_PASSWORD"),
+        connection_timeout=5, 
+        ssl_ca=os.getenv("DB_SSL"), 
+        port=os.getenv("DB_PORT"),
+        database=os.getenv("DB_DATABASE")
+    )
 
-def main():
-    print("Starting Flask server...")
+    # Create the cursor to iterate through the database
+    mycursor = mydb.cursor()
 
-    try:
-        mydb = mysql.connector.connect(
-            host=os.getenv("DB_HOSTNAME"),
-            user=os.getenv("DB_USERNAME"),
-            password=os.getenv("DB_PASSWORD"),
-            connection_timeout=5, 
-            ssl_ca=os.getenv("DB_SSL"), 
-            port=os.getenv("DB_PORT"),
-            database=os.getenv("DB_DATABASE")
-        )
+    # Testing if the database works 
+    mycursor.execute("SELECT * FROM users")
+    result = mycursor.fetchall()
 
-        # Load salt and password for hashing 
-        pwd = os.getenv("PWD")
-        s = os.getenv("SALT")
+except Exception as e:
+    print("Database error:", e)
 
-        mycursor = mydb.cursor()
-        mycursor.execute("SELECT * FROM users")
-        result = mycursor.fetchall()
+# Load salt and password for hashing 
+s = os.getenv("SALT")
 
-        print(result[0])
-
-    except Exception as e:
-        print("Database error:", e)
-
-    app.run(debug=True, port=5001)
-
-
+# App routes with functions 
 @app.route('/')
 def home():
     return render_template("login.html")
@@ -58,20 +52,62 @@ def ask_question():
     answer = agent.search_corpus(question)
     return render_template("index.html", answer=answer)
 
-@app.route('/login', methods=['POST'])
-def login(): 
-    username = request.form['username'] # get data from the HTML form 
-    password = request.form['password'] # get the data from the HTML form 
+@app.route('/login', methods=['GET','POST'])
+def login():
 
-    mycursor.execute()
- 
-@app.route('/signup', method=["POST"])
+    # make sure the route is a proper fetch or post
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        # get stored password
+        mycursor.execute("SELECT password FROM users WHERE username = %s",(username,)) # check to see whether the passwords match
+        result = mycursor.fetchone() # get the first value from the response
+        
+        if result:
+            stored_password = result[0]
+            if hashPassword(password) == stored_password:
+                return render_template("index.html") # go to the home page if successfully logged in 
+        return render_template("login.html", error="Invalid username or password") # if login wasn't successful, stay on the log in page
+    
+    return render_template('login.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        try: 
+            username = request.form['username']
+            email = request.form['email']
+            password = request.form['password']
 
-    # Get data from the HTML form
-    username = request.form['username']
-    email = request.form['email']
-    password = request.form['password']
+            # hash the password for added security
+            password = hashPassword(password)
 
+            # Insert the new account information into the database
+            mycursor.execute("INSERT INTO users (username, emailaddress, password) VALUES (%s, %s, %s)", (username, email, password))
+            # Save the new row in the database
+            mydb.commit()
+
+            # If successful login, go to the homepage
+            return render_template("index.html")
+        except Exception as e: 
+            # If there's an issue, stay on the signup page until it works
+            return render_template("signup.html", error="Soemthing went wrong, please try again.")
+    return render_template("signup.html")
+
+# Helper functions 
+def hashPassword(plainText):
+    pwd_salt = plainText+s
+    hashed = hashlib.sha256(pwd_salt.encode()).hexdigest()
+    return hashed
+
+def main():
+    print("Starting Flask server...")
+
+    app.run(debug=True, port=5001)
+
+
+
+# Run from native file
 if __name__ == "__main__":
     main()
