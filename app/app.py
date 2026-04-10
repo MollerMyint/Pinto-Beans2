c0298399bb98002a7168b124dde8cf967b690240
jessp60 <jmpinto@cpp.edu>
2026-04-10 18:03:37 -0400

    update PORT for deployment

--- app/app.py
+++ app/app.py
@@ -307,7 +307,7 @@ def parse_agent_reply(reply):
 def main():
     print("Starting Flask server...")
 
-    app.run(debug=True, port=5001, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
 
 # Run from native file
 if __name__ == "__main__":
