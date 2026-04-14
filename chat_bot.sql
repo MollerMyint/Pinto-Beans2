d7040a2f6dc07245ceb99890063144ed34c8dd97
Nataliemamikonyan <143463869+Nataliemamikonyan@users.noreply.github.com>
2026-04-13 19:42:56 -0700

    Added .sql file

---
+++ chat_bot.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE pintoBeansdb;
+
+CREATE TABLE `users` (
+  `user_id` int PRIMARY KEY AUTO_INCREMENT,
+  `username` varchar(50) UNIQUE NOT NULL,
+  `email` varchar(100) UNIQUE NOT NULL,
+  `password` varchar(255) NOT NULL
+);
+
+CREATE TABLE `chats` (
+  `chat_id` int PRIMARY KEY AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `created_at` timestamp DEFAULT (CURRENT_TIMESTAMP)
+);
+
+CREATE TABLE `messages` (
+  `message_id` int PRIMARY KEY AUTO_INCREMENT,
+  `chat_id` int NOT NULL,
+  `question` text NOT NULL,
+  `answer` text NOT NULL
+);
+
+ALTER TABLE `chats` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+ALTER TABLE `messages` ADD FOREIGN KEY (`chat_id`) REFERENCES `chats` (`chat_id`);
